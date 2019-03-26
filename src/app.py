a3a92322b0c5d3645ec97c5f0a8bb22bdbdf1afe
Douglas Hoang <douglashoang@Douglass-MacBook-Pro.local>
2019-03-26 13:43:31 -0500

    mapped motors with scales

--- src/app.py
+++ src/app.py
@@ -7,12 +7,17 @@ import threading
 
 #setting up pins
 ########THESE LINE WILL NEED TO BE ADDED TO THE MAIN CODE TO SETUP GPIO PINS########
-Motor1 = 5 #set GPIO pin thats connected to first motor
-Motor2 = 12 #set GPIO pin thats connected to second motor
-Motor3 = 6 #set GPIO pin thats connected to third motor
-Motor4 = 13 #set GPIO pin thats connected to fourth motor
-Motor5 = 19 #set GPIO pin thats connected to fifth motor
-Motor6 = 26 #set GPIO pin thats connected to sixth motor
+Motor1 = 2 #set GPIO pin thats connected to first motor
+Scale1 = [15,17]
+Motor2 = 3 #set GPIO pin thats connected to second motor
+Scale2 = [18, 27]
+Motor3 = 4 #set GPIO pin thats connected to third motor
+Scale3 = [22, 23]
+Motor4 = 5 #set GPIO pin thats connected to fourth motor
+Scale4 = [24, 25]
+Motor5 = 6 #set GPIO pin thats connected to fifth motor
+Scale5 = [26, 20]
+Motor6 = 7 #set GPIO pin thats connected to sixth motor
 Sleep = 16 #set GPIO pin thats connected to Sleep
 Step = 20 #set GPIO pin thats connected to Step
 Dir = 21 #set GPIO pin thats connected to Dir
@@ -25,21 +30,48 @@ Outs = (Motor1,Motor2,Motor3,Motor4,Motor5,Motor6,Sleep,Step,Dir)
 GPIO.setup(Outs, GPIO.OUT)
 ###################################################################################
 
+# maping the motor with the scale
+motorDict = {
+        '0': {
+            'motor': 2,
+            'scale': [15, 17]
+            },
+        '1': {
+            'motor': 3,
+            'scale': [18, 27]
+            },
+        '2': {
+            'motor': 4,
+            'scale': [22, 23]
+            },
+        '3': {
+            'motor': 5,
+            'scale': [24, 25]
+            },
+        '4': {
+            'motor': 6,
+            'scale': [19, 16]
+            },
+        '5': {
+            'motor': 7,
+            'scale': [26, 20]
+            },
+        }
 
 #motor function triggers motor on or off. E.I motor(ON,Motor1) will turn on motor 1
-def motor(toggle,motor):
+def toggle_motor(toggle,motor):
     if toggle == 1:
-        GPIO.output(motor, GPIO.HIGH)
+        GPIO.output(motorDict[motor]['motor'], GPIO.HIGH)
     if toggle == 0:
-        GPIO.output(motor, GPIO.LOW)
+        GPIO.output(motorDict[motor]['motor'], GPIO.LOW)
 
 def grindSpice(motor, amount):
     # start the motor
-    motor(motor, 1)
+    toggle_motor(motorDict[motor]['motor'], 1)
 
     # Setting up scales
     ### first param a GPIO number?
-    hx = HX711(22, 23)
+    hx = HX711(motorDict[motor]['scale'][0], motorDict[motor]['scale'][1])
     hx.set_reading_format("MSB", "MSB")
     hx.set_reference_unit(920)
 
@@ -50,6 +82,7 @@ def grindSpice(motor, amount):
         try:
             val = hx.get_weight(5)
             val = hx.read_long()
+            print "Grinder #", motor, " grinded ", val
 
             if val >= amount:
                 break
@@ -61,7 +94,7 @@ def grindSpice(motor, amount):
             print("error with the scale")
 
         # stop the motor
-        motor(motor, 0)
+        toggle_motor(motorDict[motor]['motor'], 0)
 
 class grindThread(threading.Thread):
     def __init__(self, motor, amount):
@@ -78,7 +111,7 @@ def hello(**kwargs):
 
 @dispatcher.add_method
 def toggle(**kwargs):
-    motor(kwargs['toggle'], kwargs['motor'])
+    toggle_motor(kwargs['toggle'], kwargs['motor'])
     return True
 
 @dispatcher.add_method
